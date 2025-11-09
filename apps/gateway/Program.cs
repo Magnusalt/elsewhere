1d145672a60e2f9133cc7fbc7524d1733e0b8b42
Magnus <magnus.altgard@gmail.com>
2025-11-09 22:55:09 +0100

    adds otel to other dotnet projects

--- apps/gateway/Program.cs
+++ apps/gateway/Program.cs
@@ -1,5 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using common_extensions;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.AddDefaults();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
